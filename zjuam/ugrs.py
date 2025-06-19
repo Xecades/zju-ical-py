6122ea275433f7cc9e550ea813ce732add6bb006
Xecades <i@xecades.xyz>
2025-06-19 21:45:15 +0800

    fix: zdbk now uses https instead of http

--- zjuam/ugrs.py
+++ zjuam/ugrs.py
@@ -9,8 +9,8 @@ from course.convert import ugrsClassTermToQueryString
 
 
 class UgrsZjuam(Zjuam):
-    COURSE_URL = "http://zdbk.zju.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html"
-    EXAM_URL = "http://zdbk.zju.edu.cn/jwglxt/xskscx/kscx_cxXsgrksIndex.html?doType=query&queryModel.showCount=%s"
+    COURSE_URL = "https://zdbk.zju.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html"
+    EXAM_URL = "https://zdbk.zju.edu.cn/jwglxt/xskscx/kscx_cxXsgrksIndex.html?doType=query&queryModel.showCount=%s"
     ZDBK_LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=http%3A%2F%2Fzdbk.zju.edu.cn%2Fjwglxt%2Fxtgl%2Flogin_ssologin.html"
 
     def __init__(self, username: str, password: str):
